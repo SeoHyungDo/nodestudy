e7ad6c3acf15059ffbba9b75263518dd47196982
SeoHyungDo <39759865+SeoHyungDo@users.noreply.github.com>
2023-03-01 23:05:30 +0900

    Add files via upload

--- start.js
+++ start.js
@@ -2,4 +2,10 @@ console.log("Test Text")
 
 let b=0;
 b=1;
-console.log(b)
+console.log(b);
+
+const a1 = 1;
+const a2 = 2;
+const num2 = 3;
+const string2 = `${a1} 더하기 ${a2}는 '${num2}'`;
+console.log(string2);
